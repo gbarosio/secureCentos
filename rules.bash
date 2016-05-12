8cb2363557f02f50357bb9d5f3621af865f72f08
Guido Barosio <gbarosio@gmail.com>
2016-05-11 23:09:48 -0400

    rules.bash

---
+++ rules.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-service=ssh
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --permanent --list-all
+sudo firewall-cmd --reload
+sudo systemctl enable firewalld
